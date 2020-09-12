3fb1dfa7556c96cca80a49da75a9c6082387eb07
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-12 16:39:02 -0700

    initial setup - 2

---
+++ src/globals/variables.js
@@ -0,0 +1,1 @@
+const APP_FOLDER_NAME = '/react-movie-app'
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
+import { APP_FOLDER_NAME } from '../globals/variables';
+import Header from '../components/About';
+import Home from '../components/Home';
+import About from '../components/Footer';
+import Footer from '../components/Footer';
+import MyMovies from '../components/MyMovies';
+
+
+
+
+const AppRouter = () => (
+    <Router basename={APP_FOLDER_NAME}>
+		<div className="wrapper">
+			<Switch>
+				<Route path={'/'} exact><Home /></Route>
+			</Switch>
+		</div>
+	</Router>
+)
+
+export default AppRouter;
