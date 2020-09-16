3b9cfc6dea0c0503dbc13a6ae75aa3c58ebca329
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-16 14:48:07 -0700

    scss edit

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => (
         Know us
         </h2>
         <p>
-        The reel app is an online database, designed and created by Heyam Banitorof and Vidhi Shukla, which allows you to search through movies and see the information related to them online.</p> 
+        The Reels App is an online database, designed and created by Heyam Banitorof and Vidhi Shukla, which allows you to search through movies and see the information related to them online.</p> 
         <p>
         Heyam and Vidhi are both web developer and UX/UI designer based in Vancouver. They both had different backgrounds and experiences before they changed their career to web development. Their goal now is to make web a better place for everyone.
 
