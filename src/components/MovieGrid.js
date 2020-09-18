2f427b23187ee4f8e563e5308a5d7f0511d9d5f6
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-18 10:15:13 -0700

    edit layout movieGrid

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -10,12 +10,13 @@ const MovieGrid = ({movie}) => {
                 <div className={`movie-0${i + 1}`} key={i}>
                 <div className='movie-poster'>
                 <figure></figure>
-                </div>
+                </div> 
+                 <img className="poster" src={IMAGE_URL + movie.poster_path} />
                 <div className="title">{movie.title}</div>
                 <div className='release-date'>{movie.release_date}</div>
                 <div className='rating'>{movie.vote_average}</div>
                 <div className='summary'>{movie.overview}</div>
-                <img className="poster" src={IMAGE_URL + movie.poster_path} />
+              
            
                 </div>
                 );
