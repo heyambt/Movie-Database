e1aab28e6266e293a8d35580babd26ae0c85f52a
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-18 21:41:20 -0700

    scss edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,7 +65,7 @@ const handleChangeSelection = (e) => {
 			</form> 
             </div>
 		</section>
-        
+      
      {movieData && <MovieGrid movie={movieData} />}
         
         </div>  
