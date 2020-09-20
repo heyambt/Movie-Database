e4a09a8a0be7d9b263ba6235b45790904b705a71
heyam <hiam.bt@gmail.com>
2020-09-20 12:31:21 -0700

    styles- body- main-footer
    styles for Desktop and Mobile view

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,14 @@ const About = () => (
         <h2>
         Know us
         </h2>
+        <div className='about'>
         <p>
         The Reels App is an online database, designed and created by Heyam Banitorof and Vidhi Shukla, which allows you to search through movies and see the information related to them online.</p> 
         <p>
         Heyam and Vidhi are both web developer and UX/UI designer based in Vancouver. They both had different backgrounds and experiences before they changed their career to web development. Their goal now is to make web a better place for everyone.
 
         </p>
+        </div>
     </main>
 );
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const d = new Date();
 const y = d.getFullYear();
 
 const Footer = () => (
-    <footer>
+    <footer className='footer'>
         <p>Created by Heyam Banitorof and Vidhi Shukla &copy;{y}</p>
     </footer>
 );
