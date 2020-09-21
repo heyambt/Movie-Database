3b3fcae1fdd311f472b3893e89651d8a72564545
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-20 22:46:59 -0700

    singleMovie add

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import MovieGrid from './MovieGrid';
+import { API_KEY, API_KEY_ONLY, API_URL, IMAGE_URL} from '../globals/variables';
+
 
 const SingleMovie = () => {
+    const initialMovieData = [];
+
+    const[movieDetails, setMovieDetails] = useState(null)
 
     let {id} = useParams();
     console.log(id)
-    
-   
 
+    useEffect(() => {
+        const fetchDetails = async () => {
+            // Make our API call here...
+            const res = await fetch(API_URL + 'movie/'+id+'?api_key='+API_KEY_ONLY+'&language=en-US');
+            let data = await res.json(); 
+            console.log(data);
+            initialMovieData.push(data);
+            setMovieDetails(initialMovieData);
+    
+        }     
+        fetchDetails(); 
+    },[])
    
     return(
             <section className='single-movie-section'>
-                    <div className='single-movie'>
-                        {id} 
-                    </div>
-                 
-                        
-                    {/* call for single movie from tmdb */}
+                <div className='single-movie'>
+                    {movieDetails && movieDetails.map((movie) => {
+                        return (
+                            <ul>   
+                                <li>{movie.title}</li>
+                                <li>{movie.vote_average}</li>
+                                <li>{movie.release_date}</li> 
+                                <li>{movie.overview}</li>
+                                <img className="poster" src={IMAGE_URL + movie.poster_path} alt='movie-poster'/>
+                                <li>{movie.runtime}</li>
+                            </ul>
+                        )
+                    })}                   
+            </div>
 					
-            </section>
-            
-       
+        </section>              
 
     );
     
