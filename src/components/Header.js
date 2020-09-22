6e35eeb3a917ea1d9a0d897d6428bbba7fef16b1
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-22 00:27:44 -0700

    temp Fav

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,12 +11,6 @@ const Header = () => (
            <Link to={'/'}> <img className='logo' src = {movieLogo} alt="Logo" /></Link>
             <div className="menu">
 		    <nav >
-<<<<<<< HEAD
-                <ul>
-                    <li><NavLink to={'/'} exact>Home</NavLink></li>
-                    <li><NavLink to={'/about'}>About</NavLink></li>
-                    <li><NavLink to={'/favorites'}>Favorites</NavLink></li>
-=======
                 {/* <button className='hamburger' id='hamburger'>
                     <i className='fas fa-bars'></i>
                 </button> */}
@@ -24,8 +18,7 @@ const Header = () => (
                 <ul className='nav-ul' id='nav-ul'>
                     <li className='home'><NavLink  to={'/'} exact>Home</NavLink></li>
                     <li className='about'><NavLink to={'/about'}>About</NavLink></li>
-                    <li className='favourites'><NavLink to={'/favourites'}>Favourites</NavLink></li>
->>>>>>> 19cb36aa9faa2bb094d5a01d4599ba9b33fdde29
+                    <li className='favorites'><NavLink to={'/favorites'}>Favorites</NavLink></li>
                 </ul>
 	        </nav>
             </div>
