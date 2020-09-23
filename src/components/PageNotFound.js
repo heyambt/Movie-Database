189bf87b0944c11f90d89b7e7a6df263b0057144
Vidhi Shukla <vidhitrivedi017@gmail.com>
2020-09-22 21:22:27 -0700

    change fav page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,8 +5,8 @@ const PageNotFound = () => (
 	<main>
 		<section className="home-button">
 			<h2>404 ... : (</h2>
-			<p>Page not found.</p>
-			<p>Go to <Link to= "/"><button>Home</button></Link></p>
+			<p>Sorry you have no favourited movies. Return to the home page to add a favourite movie.</p>
+			<p><Link to= "/"><button>Home</button></Link></p>
 		</section>
 	</main>
 );
