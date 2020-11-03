fb751e254f7cfdc3f8708c12b82d2d47464d62dc
heyam <hiam.bt@gmail.com>
2020-11-02 19:02:16 -0800

    code organizing

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -32,54 +32,37 @@ const MovieGrid = ({movie}) => {
 
     const movieDivs = (arr) => {
             return arr.map((movie,i) => {
-            
             // const getFavs = getStorage()
-
             let indexFetched = faveMovies.findIndex(function(item){
                 return item.id == movie.id
             });
-        
              console.log(indexFetched);
-
             return(
                 <div className="movies-list">
-                <div className={`movie-0${i + 1}`} key={i}>
-      
-                <img className="poster" src={IMAGE_URL + movie.poster_path} alt='movie-poster'/>
-                  
-                <div className="title">{movie.title}   </div>
-                
-                <div className='rating'>{movie.vote_average}</div>
-                <div className='release-date'>{movie.release_date}</div>
-                <div className='summary'><p>{movie.overview}</p></div>
-              
+                    <div className={`movie-0${i + 1}`} key={i}>
+                    <img className="poster" src={IMAGE_URL + movie.poster_path} alt='movie-poster'/> 
+                    <div className="title">{movie.title}   </div>               
+                    <div className='rating'>{movie.vote_average}</div>
+                    <div className='release-date'>{movie.release_date}</div>
+                    <div className='summary'><p>{movie.overview}</p></div>             
                 </div>
-
                 <div className="movie-info">
-                <div className='link-to-moreinfo'><Link to={'/singlemovie/' + movie.id}>
-                   
-                   <button className='more-info'>More Info</button></Link></div>
-                   <div className='single-fav'>
-                        {indexFetched >= 0 ? (
-                            <button className='remove-fav'onClick={() => {removeFromFavorite(movie)}}>Remove Favorite</button>
-                         ):
-                         (
-                            <button className='add-fav'onClick={()=> {handleAddFavorite(movie)}} style={{color: 'white'}}>Add to Favorites</button>
-                        )}
-                    </div>
-                
-                
-                 
-                     
-                </div>
-                
+                    <div className='link-to-moreinfo'><Link to={'/singlemovie/' + movie.id}>   
+                    <button className='more-info'>More Info</button></Link></div>
+                    <div className='single-fav'>
+                            {indexFetched >= 0 ? (
+                                <button className='remove-fav'onClick={() => {removeFromFavorite(movie)}}>Remove Favorite</button>
+                            ):
+                            (
+                                <button className='add-fav'onClick={()=> {handleAddFavorite(movie)}} style={{color: 'white'}}>Add to Favorites</button>
+                            )}
+                    </div>                   
+                    </div>               
                 </div>
                 );
             });
         }
-        
-
-        return (
+      return (
             <div className="twelve-movie-grid">
                 {movieDivs(movie)}
                 
