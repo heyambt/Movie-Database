bcfc46c8e941630989ee096286fbc5e0a9e1ad2c
heyam <hiam.bt@gmail.com>
2020-11-12 20:36:41 -0800

    removing / from API

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -1,7 +1,7 @@
 
 export const API_KEY_ONLY = 'fd4d70b7c00eea5374fcc254ca03e285';
 export const API_KEY = `&appid=${API_KEY_ONLY}`;
-export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_URL = 'https://api.themoviedb.org/3';
 export const API_DATA_MODE = '&mode=json';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/w200/';
 
